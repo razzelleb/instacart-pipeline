70f3da31d3c7c9c8c69000cbfd7aaaedad6ee7bd
razzelle.boreros@ftwfoundation.org <razzelle.boreros@ftwfoundation.org>
2026-09-02 14:58:42 +0000

    feature/gold dim_product
    Used left join on clean department and products table
    Commented possible merge into version of the code

---
+++ src/sql/03_mart/01_dim_product.sql
@@ -0,0 +1,36 @@
+-- Dim Product Table
+-- Join clean products and departments into the mart dimension table
+
+CREATE OR REPLACE TABLE instacart.instacart_mart.dim_product AS
+SELECT 
+    p.product_id,
+    p.product_name,
+    p.department_id,
+    d.department
+FROM instacart.instacart_clean.products p
+LEFT JOIN instacart.instacart_clean.departments d
+    ON p.department_id = d.department_id;
+
+
+
+/*Merge Into Alternate Code - not yet checked
+MERGE INTO instacart.instacart_mart.dim_product AS target
+USING (
+    SELECT 
+        p.product_id,
+        p.product_name,
+        p.department_id,
+        d.department
+    FROM instacart.instacart_clean.products p
+    LEFT JOIN instacart.instacart_clean.departments d
+        ON p.department_id = d.department_id
+) AS source
+ON target.product_id = source.product_id
+WHEN MATCHED THEN
+    UPDATE SET 
+        target.product_name = source.product_name,
+        target.department_id = source.department_id,
+        target.department = source.department
+WHEN NOT MATCHED THEN
+    INSERT (product_id, product_name, department_id, department)
+    VALUES (source.product_id, source.product_name, source.department_id, source.department); */
