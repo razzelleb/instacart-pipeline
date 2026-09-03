1787e3b384a37ec564e63c01b2962d7f3225dca6
hazelle.cuevas@ftwfoundation.org <hazelle.cuevas@ftwfoundation.org>
2026-09-03 07:10:32 +0000

    kpi summary based on new facts table

--- src/sql/04_visualization/05_.sql
+++ src/sql/04_visualization/05_.sql
@@ -6,16 +6,4 @@ SELECT
         2
     ) AS reorder_rate,
     COUNT(*) AS total_products_purchased
-FROM (
-    SELECT
-        order_id,
-        product_id,
-        reordered
-    FROM instacart.instacart_clean.order_products_prior
-    UNION ALL
-    SELECT
-        order_id,
-        product_id,
-        reordered
-    FROM instacart.instacart_clean.order_products_train
-) AS all_orders;
+FROM instacart.instacart_clean.fact_order_products;
