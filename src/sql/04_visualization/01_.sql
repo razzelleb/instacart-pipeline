db0b69991d18dfdd5cf73a6ef6aaf99671cb004c
razzelle.boreros@ftwfoundation.org <razzelle.boreros@ftwfoundation.org>
2026-09-03 06:35:31 +0000

    Added s on FROM line

--- src/sql/04_visualization/01_.sql
+++ src/sql/04_visualization/01_.sql
@@ -3,7 +3,7 @@ SELECT
     p.product_name AS product,
     p.department AS department,
     COUNT(f.order_id) AS total_purchase
-FROM instacart.instacart_mart.fact_order_product AS f
+FROM instacart.instacart_mart.fact_order_products AS f
 LEFT JOIN instacart.instacart_mart.dim_product AS p
     ON f.product_id = p.product_id
 GROUP BY product, department
