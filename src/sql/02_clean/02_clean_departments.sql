66f2a75d51888ca46ee2b57594dc329e723051c6
hazelle.cuevas@ftwfoundation.org <hazelle.cuevas@ftwfoundation.org>
2026-09-03 22:47:46 +0000

    feat: update clean departments for uniformity (INITCAP)

--- src/sql/02_clean/02_clean_departments.sql
+++ src/sql/02_clean/02_clean_departments.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE TABLE instacart.instacart_clean.departments AS
 SELECT
     CAST(department_id AS INT) AS department_id,
-    LOWER(TRIM(department)) AS department
+    INITCAP(TRIM(department)) AS department
 FROM instacart.instacart_raw.departments
 WHERE department_id IS NOT NULL
   AND department IS NOT NULL
