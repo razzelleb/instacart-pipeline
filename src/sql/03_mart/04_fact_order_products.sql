eaf3f93056570fa3aa4d2f8fd8e0f230f0f9f107
razzelle.boreros@ftwfoundation.org <razzelle.boreros@ftwfoundation.org>
2026-09-03 07:05:24 +0000

    Update on the facts table to add reordered

--- src/sql/03_mart/04_fact_order_products.sql
+++ src/sql/03_mart/04_fact_order_products.sql
@@ -3,12 +3,21 @@ SELECT
     op.order_id,
     op.product_id,
     p.department_id,
-    p.aisle_id
+    p.aisle_id,
+    op.reordered
 FROM (
-    SELECT order_id, product_id
+    SELECT
+        order_id,
+        product_id,
+        reordered
     FROM instacart.instacart_clean.order_products_prior
+
     UNION ALL
-    SELECT order_id, product_id
+
+    SELECT
+        order_id,
+        product_id,
+        reordered
     FROM instacart.instacart_clean.order_products_train
 ) op
 LEFT JOIN instacart.instacart_clean.products p
