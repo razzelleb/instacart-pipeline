f2333ab6d36c3646a7b67719daa5e4707f5b3639
hazelle.cuevas@ftwfoundation.org <hazelle.cuevas@ftwfoundation.org>
2026-09-03 22:45:47 +0000

    feat: update clean aisles (INITCAP) for uniformity

--- src/sql/02_clean/01_clean_aisles.sql
+++ src/sql/02_clean/01_clean_aisles.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE TABLE instacart.instacart_clean.aisles AS
 SELECT
     CAST(aisle_id AS INT) AS aisle_id,
-    LOWER(TRIM(aisle)) AS aisle
+    INITCAP(TRIM(aisle)) AS aisle
 FROM instacart.instacart_raw.aisles
 WHERE aisle_id IS NOT NULL
   AND aisle IS NOT NULL
